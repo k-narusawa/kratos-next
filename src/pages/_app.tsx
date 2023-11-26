335b4d26943a2466cb3d95855ce1b72c28b2563e
k-narusawa <swkei320@gmail.com>
2023-11-26 18:28:25 +0900

    refactor: 💡 ヘッダーを出す条件を設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
 import Header from '@/src/components/base/Header'
+import Spinner from '@/src/components/ui/Spinner'
+import useSession from '@/src/hooks/useSession'
 import '@/src/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { session, isLoading } = useSession()
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (!session) {
+    return (
+      <div className='bg-gray-50 dark:bg-gray-900'>
+        <Component {...pageProps} />
+      </div>
+    )
+  }
+
   return (
     <>
       <Header />
