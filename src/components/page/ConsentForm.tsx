7d9fb2ca4dc7dd34d200b6d930dfaade97f3c1aa
k-narusawa <swkei320@gmail.com>
2024-01-03 20:33:50 +0900

    feat: 🎸 同意画面の側だけ

---
+++ src/components/page/ConsentForm.tsx
@@ -0,0 +1,62 @@
+import Button from '@/src/components/ui/Button'
+import Card from '@/src/components/ui/Card'
+import { OAuth2ConsentRequest } from '@ory/client'
+import React, { FormEventHandler } from 'react'
+import { useTranslation } from 'react-i18next'
+
+interface ConsentFormProps {
+  handleAccept: FormEventHandler<HTMLFormElement>
+  consentRequest: OAuth2ConsentRequest
+}
+
+const ConsentForm: React.FC<ConsentFormProps> = ({
+  handleAccept: handleAccept,
+  consentRequest: consentRequest
+}) => {
+  const { t } = useTranslation('common')
+
+  return (
+    <Card>
+      <form onSubmit={handleAccept}>
+        <h5
+          className='
+            text-2xl font-semibold text-center 
+            text-gray-900 dark:text-white
+            mb-10 mt-5
+          '
+        >
+          {t('consent.title')}
+        </h5>
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <tbody>
+                {consentRequest.requested_scope?.map((scope, index) => (
+                <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td className="w-4 p-4">
+                    <div className="flex items-center">
+                        <input type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                        <label className="sr-only">checkbox</label>
+                    </div>
+                </td>
+                <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                    <div className="ps-3">
+                        <div className="text-base font-semibold">{t(`ory.consent.scope.${scope}`)}</div>
+                        <div className="font-normal text-gray-500">{t(`ory.consent.detail.${scope}`)}</div>
+                    </div>
+                </th>
+              </tr>
+                ))}
+              </tbody>
+            </table>
+        </div>
+        <div className='flex flex-col items-center'>
+          <Button type='submit' className='px-4 py-2 mt-5 w-8/12'>
+            {t('consent.accept')}
+          </Button>
+        </div>
+      </form>
+    </Card>
+  )
+}
+
+export default ConsentForm
--- src/pages/consent.tsx
+++ src/pages/consent.tsx
@@ -1,10 +1,11 @@
 import { oauth } from '@/pkg/sdk'
+import ConsentForm from '@/src/components/page/ConsentForm'
 import Button from '@/src/components/ui/Button'
+import Spinner from '@/src/components/ui/Spinner'
 import { useHandleError } from '@/src/hooks/useHandleError'
 import { OAuth2ConsentRequest } from '@ory/client'
 import { AxiosError } from 'axios'
 import { GetStaticProps } from 'next'
-import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
@@ -12,23 +13,19 @@ import { useEffect, useState } from 'react'
 const ConsentPage = ({}) => {
   const router = useRouter()
   const [consentRequest, setConsentRequest] = useState<OAuth2ConsentRequest>()
-  const handleError = useHandleError()
-  const { t } = useTranslation('common')
+  const handleError = useHandleError()  
 
   const { consent_challenge: consentChallenge } = router.query
 
   useEffect(() => {
-    console.log('consentChallenge', consentChallenge)
     if (!consentChallenge) {
       return
     } else {
-      console.log('consentChallenge', consentChallenge)
       oauth
         .getOAuth2ConsentRequest({
           consentChallenge: consentChallenge.toString(),
         })
         .then(({ data }) => {
-          console.log('getOAuth2ConsentRequest', data)
           setConsentRequest(data)
         })
         .catch((err: AxiosError) => handleError(err))
@@ -58,53 +55,21 @@ const ConsentPage = ({}) => {
     }
   }
 
-  const handleReject = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
-    if (!consentRequest) {
-      return
-    } else {
-      await oauth
-        .rejectOAuth2ConsentRequest({
-          consentChallenge: consentRequest.challenge.toString(),
-        })
-        .then(({ data }) => {
-          if (data.redirect_to) {
-            router.push(data.redirect_to)
-          }
-        })
-        .catch((err: AxiosError) => handleError(err))
-    }
-  }
-
   if (!consentRequest) {
     return (
-      <div>
-        <h1>Loading...</h1>
+      <div className='flex items-center justify-center h-screen'>
+        <Spinner />
+        <h2 className='ml-4'>Flow not found</h2>
       </div>
     )
   }
 
   return (
-    <div>
-      <h1>Consent</h1>
-      <form onSubmit={handleAccept}>
-        <Button
-          type='submit'
-          className='w-full px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600'
-        >
-          {t('consent.accept')}
-        </Button>
-      </form>
-      <form onSubmit={handleReject}>
-        <Button
-          type='submit'
-          className='w-full px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600'
-        >
-          {t('consent.reject')}
-        </Button>
-      </form>
-    </div>
+    <>
+        <div className='flex items-center justify-center h-screen'>
+          <ConsentForm handleAccept={handleAccept} consentRequest={consentRequest}/>
+        </div>
+      </>
   )
 }
 
